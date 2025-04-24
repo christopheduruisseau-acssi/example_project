c1cf73cb5770a0adc99c8f30c8ef9e83f44f935a
christopheduruisseau-acssi <christopheduruisseau-acssi@users.noreply.github.com>
2025-04-24 15:14:21 +0000

    style fixes by ruff

--- python_sample.py
+++ python_sample.py
@@ -1,14 +1,10 @@
 from typing import Iterable
 
-import os
 
-#Projet test python 
-#New dev environement
+# Projet test python
+# New dev environement
 
 
 def sum_even_numbers(numbers: Iterable[int]) -> int:
     """Given an iterable of integers, return the sum of all even numbers in the iterable."""
-    return sum(
-        num for num in numbers
-        if num % 2 == 0
-    )
+    return sum(num for num in numbers if num % 2 == 0)
