0800263eecf4a032c37049465196ca3988393db8
Christophe <cduruisseau@acssi.fr>
2025-04-24 16:34:44 +0200

    add comment

--- python_sample.py
+++ python_sample.py
@@ -2,6 +2,7 @@ from typing import Iterable
 
 import os
 
+#Projet test python
 
 def sum_even_numbers(numbers: Iterable[int]) -> int:
     """Given an iterable of integers, return the sum of all even numbers in the iterable."""
